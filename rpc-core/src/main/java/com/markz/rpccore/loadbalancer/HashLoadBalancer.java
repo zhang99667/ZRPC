e63a96361b760f7993d9a67dab8d074b01ede6e0
markz <=>
2024-10-19 19:55:43 +0800

    IP Hash 负载均衡器

---
+++ rpc-core/src/main/java/com/markz/rpccore/loadbalancer/HashLoadBalancer.java
@@ -0,0 +1,25 @@
+package com.markz.rpccore.loadbalancer;
+
+import cn.hutool.core.net.NetUtil;
+import com.markz.rpccore.model.ServiceMeta;
+
+import java.util.LinkedHashSet;
+import java.util.List;
+
+/**
+ * IP Hash 负载均衡器
+ */
+public class HashLoadBalancer implements LoadBalance {
+
+    @Override
+    public ServiceMeta selectServer(List<ServiceMeta> serviceMetaList) {
+        LinkedHashSet<String> ipv4s = NetUtil.localIpv4s();
+        Object[] ipv4Array = ipv4s.stream().toArray();
+        String ipv4 = (String) ipv4Array[0];
+
+        int ipHash = ipv4.hashCode();
+        // hashcode 可能是负数，所以取个绝对值
+        int index = Math.abs(ipHash % serviceMetaList.size());
+        return serviceMetaList.get(index);
+    }
+}
