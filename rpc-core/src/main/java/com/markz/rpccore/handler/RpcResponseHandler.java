46114935ed93961ec224d2b071c7c1882ba54988
markz <=>
2024-10-20 10:53:08 +0800

    接受 Response

---
+++ rpc-core/src/main/java/com/markz/rpccore/handler/RpcResponseHandler.java
@@ -0,0 +1,30 @@
+package com.markz.rpccore.handler;
+
+import com.markz.common.entity.rpc.RpcResponse;
+import com.markz.rpccore.holder.RpcRequestHolder;
+import com.markz.rpccore.request.RequestPromise;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 接受 Response
+ */
+@Slf4j
+public class RpcResponseHandler extends SimpleChannelInboundHandler<RpcResponse> {
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        log.info("连接就绪");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, RpcResponse rpcResponse) {
+        log.info("收到响应：{}", rpcResponse);
+        RequestPromise requestPromise = RpcRequestHolder.getRequestPromise(rpcResponse.getRequestId());
+        if (requestPromise != null) {
+            // setSuccess 结束阻塞
+            requestPromise.setSuccess(rpcResponse);
+        }
+    }
+}
