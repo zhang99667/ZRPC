9fd937822048e5b95fbad1825002c1f1096e1b68
markz <=>
2024-10-20 17:00:14 +0800

    修复由反序列化导致的通道异常致命bug

--- rpc-core/src/main/java/com/markz/rpccore/bootstrap/service/initializer/RpcServiceInitializer.java
+++ rpc-core/src/main/java/com/markz/rpccore/bootstrap/service/initializer/RpcServiceInitializer.java
@@ -1,6 +1,6 @@
 package com.markz.rpccore.bootstrap.service.initializer;
 
-import com.markz.rpccore.codec.fastjson.FastJsonDecoder;
+import com.markz.rpccore.codec.RpcRequestDecoder;
 import com.markz.rpccore.codec.fastjson.FastJsonEncoder;
 import com.markz.rpccore.codec.frame.FrameDecoder;
 import com.markz.rpccore.codec.frame.FrameEncoder;
@@ -11,13 +11,14 @@ import io.netty.channel.ChannelInitializer;
 // 服务端 pipeline
 public class RpcServiceInitializer extends ChannelInitializer {
     @Override
-    protected void initChannel(Channel ch) throws Exception {
+    protected void initChannel(Channel ch) {
         // outbound handler
         ch.pipeline().addLast("FrameEncoder", new FrameEncoder()); // 封装成帧
         ch.pipeline().addLast("Serializer", new FastJsonEncoder()); // 序列化
         // inbound handler
         ch.pipeline().addLast("FrameDecoder", new FrameDecoder()); // 拆帧
-        ch.pipeline().addLast("Deserializer", new FastJsonDecoder()); // 反序列化
+        // TODO 序列化器这里需要后续根据协议的type自动处理
+        ch.pipeline().addLast("Deserializer", new RpcRequestDecoder()); // 反序列化
         ch.pipeline().addLast("RpcRequestHandler", new RpcRequestHandler()); // 处理 RpcRequest
     }
 }
