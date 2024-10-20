d8b49bd100984a822ec3313a636f942310dcf497
markz <=>
2024-10-20 10:56:31 +0800

    UserService 实现类

---
+++ rpc-provider/src/main/java/com/markz/provider/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.markz.provider.service.impl;
+
+import com.markz.common.entity.User;
+import com.markz.common.service.UserService;
+import com.markz.rpccore.annotation.RpcService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RpcService
+public class UserServiceImpl implements UserService {
+    @Override
+    public User getUserById(Integer id) {
+        User user = new User();
+        user.setId(1);
+        user.setUsername("markz");
+        user.setPassword("1234");
+        return user;
+    }
+
+    @Override
+    public String test() {
+        return "hello";
+    }
+}
