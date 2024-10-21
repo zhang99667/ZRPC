b0f6e676ca55ac9c8e7633d359c9b298ac2fe3e4
markz <=>
2024-10-21 08:43:23 +0800

    服务提供者元数据，添加服务名称

--- rpc-core/src/main/java/com/markz/rpccore/model/ServiceProviderMeta.java
+++ rpc-core/src/main/java/com/markz/rpccore/model/ServiceProviderMeta.java
@@ -4,9 +4,15 @@ import lombok.Data;
 
 /**
  * 服务提供者元信息
+ * key: /业务前缀/服务名/服务节点地址
  */
 @Data
 public class ServiceProviderMeta {
+
+    /**
+     * 服务名称
+     */
+    private String serviceName;
 
     /**
      * 主机名
@@ -22,4 +28,13 @@ public class ServiceProviderMeta {
      * 服务提供者权重，用于 loadbalancer
      */
     private Integer weight;
+
+    /**
+     * 返回 key
+     * 格式：/rpc/com.markz.Service.UserService/localhost:8081
+     * @return
+     */
+    public String getServiceKey() {
+        return String.format("%s/%s:%s", serviceName, serviceHost, servicePort);
+    }
 }
