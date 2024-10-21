23f9068c7457b9a83db8278c9e4d7f1399cdca75
markz <=>
2024-10-21 08:42:42 +0800

    更新类名

--- rpc-core/src/test/java/com/markz/rpccore/ymlReadTest.java
+++ rpc-core/src/test/java/com/markz/rpccore/ymlReadTest.java
@@ -1,6 +1,6 @@
 package com.markz.rpccore;
 
-import com.markz.rpccore.config.RpcConfig;
+import com.markz.rpccore.config.RpcConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,11 +9,11 @@ import javax.annotation.Resource;
 @SpringBootTest
 public class ymlReadTest {
     @Resource
-    private RpcConfig rpcConfig;
+    private RpcConfiguration rpcConfiguration;
 
     @Test
     void test() {
-        System.out.println("rpcConfig.getName() = " + rpcConfig.getName());
-        System.out.println("rpcConfig.getVersion() = " + rpcConfig.getVersion());
+        System.out.println("rpcConfig.getName() = " + rpcConfiguration.getName());
+        System.out.println("rpcConfig.getVersion() = " + rpcConfiguration.getVersion());
     }
 }
