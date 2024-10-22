e997c006cba723fc4a40f12215162c2873a33a28
markz <=>
2024-10-22 20:13:36 +0800

    rpc实体类

---
+++ rpc-core/src/main/java/com/markz/rpccore/constant/RpcConstant.java
@@ -0,0 +1,9 @@
+package com.markz.rpccore.constant;
+
+public class RpcConstant {
+
+    /**
+     * yml 配置文件
+     */
+    public static final String CONFIG_FILE_PATH = "src/main/resources/application.yml";
+}
