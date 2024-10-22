84a0012e9864f7116e0e7f34a406b359d7ac3579
markz <=>
2024-10-22 20:12:57 +0800

    使用Spring监听器机制完成Rpc服务端启动，摒弃异步方式

--- rpc-provider/src/main/java/com/markz/provider/ProviderApplication.java
+++ rpc-provider/src/main/java/com/markz/provider/ProviderApplication.java
@@ -1,27 +1,15 @@
 package com.markz.provider;
 
-import com.markz.rpccore.bootstrap.service.NettyRpcService;
-import com.markz.rpccore.bootstrap.service.RpcServiceBootstrap;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import javax.annotation.Resource;
 
 @SpringBootApplication
 @ComponentScan(value = "com.markz")
-public class ProviderApplication implements CommandLineRunner {
-
-    @Resource
-    private RpcServiceBootstrap rpcServiceBootstrap;
+public class ProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        rpcServiceBootstrap.run();
     }
 }
