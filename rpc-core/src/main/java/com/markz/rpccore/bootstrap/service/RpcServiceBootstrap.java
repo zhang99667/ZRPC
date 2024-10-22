83fc0c04ffd8e3bd5f9a1abdf781759538afa7b1
markz <=>
2024-10-22 11:14:51 +0800

    完善启动服务

--- rpc-core/src/main/java/com/markz/rpccore/bootstrap/service/RpcServiceBootstrap.java
+++ rpc-core/src/main/java/com/markz/rpccore/bootstrap/service/RpcServiceBootstrap.java
@@ -1,5 +1,8 @@
 package com.markz.rpccore.bootstrap.service;
 
+import com.markz.rpccore.config.RpcConfiguration;
+import com.markz.rpccore.registry.zookeeper.ZookeeperClient;
+import com.markz.rpccore.spring.RegisterRpcServices;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.Resource;
@@ -14,10 +17,22 @@ public class RpcServiceBootstrap {
     @Resource
     private NettyRpcService rpcService;
 
+    // @Resource
+    // private ZookeeperClient zkClient;
+
+    @Resource
+    private RegisterRpcServices registerRpcServices;
+
+    @Resource
+    private RpcConfiguration rpcConfiguration;
+
     public void run() throws InterruptedException {
-        // TODO 注册中心
-        // 2. start server by netty
-        rpcService.startServer(8090);
+        // // 1. 连接注册中心
+        // zkClient.init();
+        // // 2. 注册服务
+        registerRpcServices.registerServices();
+        // 3. start server by netty
+        rpcService.startServer(rpcConfiguration.getServerPort());
     }
 
 }
