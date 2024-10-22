b6fa801672d9184394be73acb5470850ce751fc0
markz <=>
2024-10-22 16:44:15 +0800

    序列化器工厂

--- rpc-core/src/main/java/com/markz/rpccore/serializer/SerializerFactory.java
+++ rpc-core/src/main/java/com/markz/rpccore/serializer/SerializerFactory.java
@@ -1,9 +1,29 @@
 package com.markz.rpccore.serializer;
 
+import com.markz.rpccore.serializer.fastjson.FastJsonSerializer;
+import com.markz.rpccore.util.SpiLoader;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
 public class SerializerFactory {
 
-    public static Serializer getSerializer() {
-        // TODO 这里还没弄
-        return null;
+    static {
+        SpiLoader.load(Serializer.class);
+    }
+
+    /**
+     * 默认序列化器
+     */
+    private static final Serializer DEFAULT_SERIALIZER = new FastJsonSerializer();
+
+    /**
+     * 获取序列化器实例
+     *
+     * @param key key
+     * @return instance
+     */
+    public static Serializer getSerializer(String key) {
+        return SpiLoader.getInstance(Serializer.class, key);
     }
 }
