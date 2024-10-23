766c1805143fa492defed7ada08116811b555e9a
markz <=>
2024-10-23 17:06:57 +0800

    Rpc Client 入口

--- rpc-core/src/main/java/com/markz/rpccore/bootstrap/client/RpcClientBootstrap.java
+++ rpc-core/src/main/java/com/markz/rpccore/bootstrap/client/RpcClientBootstrap.java
@@ -1,13 +1,51 @@
 package com.markz.rpccore.bootstrap.client;
 
+import com.markz.rpccore.config.RegistryConfiguration;
+import com.markz.rpccore.config.RpcConfiguration;
+import com.markz.rpccore.holder.RpcConfigurationHolder;
+import com.markz.rpccore.registry.Registry;
+import com.markz.rpccore.registry.RegistryFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
-import org.springframework.context.annotation.Configuration;
+import javax.annotation.Resource;
 
-@Configuration
-public class RpcClientBootstrap {
+/**
+ * 客户端启动类
+ */
+@Slf4j
+@Component
+public class RpcClientBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Resource
+    private RegistryConfiguration registryConfiguration;
+
+    @Resource
+    private RpcConfiguration rpcConfiguration;
+
     /**
-     * 客户端入口
-     * 1. 服务发现
-     * 2. 更新缓存
+     * rpc client 入口
+     * 1. 保存配置文件
+     * 2. 连接注册中心
      */
+    public void run() {
+        // 1. 保存配置文件
+        RpcConfigurationHolder.addRegistryConfiguration(registryConfiguration);
+        RpcConfigurationHolder.addRpcConfiguration(rpcConfiguration);
+
+        // 2. 连接注册中心
+        String registryType = registryConfiguration.getType();
+        Registry registry = RegistryFactory.getRegistry(registryType);
+        registry.init();
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        // 客户端的启动方式
+        if ("client".equals(rpcConfiguration.getCors())) {
+            run();
+        }
+    }
 }
