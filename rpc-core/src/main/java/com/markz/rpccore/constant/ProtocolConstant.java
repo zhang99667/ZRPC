c8602d94d0cd5a9d2f0e2b75e22f1674d413123c
markz <=>
2024-10-25 09:26:22 +0800

    添加自定义协议内容

--- rpc-core/src/main/java/com/markz/rpccore/constant/ProtocolConstant.java
+++ rpc-core/src/main/java/com/markz/rpccore/constant/ProtocolConstant.java
@@ -1,22 +1,45 @@
 package com.markz.rpccore.constant;
 
 /**
- * 一次解码器 Frame 常量
+ * 一次解码器 Frame 常量（传输协议）
  * 两端要商量好一致
  */
 public class ProtocolConstant {
+
+    /**
+     * 消息头长度 Byte
+     */
+    public static final Integer MESSAGE_HEADER_LENGTH = 14;
+
+    /**
+     * 魔数
+     */
+    public static final byte PROTOCOL_MAGIC = 0x1;
+
 
     /**
      * 最大帧长度
      */
     public static final Integer MAX_FRAME_LENGTH = Integer.MAX_VALUE;
 
+    /**
+     * 长度字段的偏移量
+     */
     public static final Integer LENGTH_FIELD_OFFSET = 0;
 
+    /**
+     * 长度字段的长度
+     */
     public static final Integer LENGTH_FIELD_LENGTH = 4;
 
+    /**
+     * 长度字段的调整值
+     */
     public static final Integer LENGTH_ADJUSTMENT = 0;
 
-    // 跳过长度字段后的初始读取位置
+    /**
+     * 跳过长度字段后的初始读取位置
+     */
     public static final Integer INITIAL_BYTES_TO_STRIP = 4;
+
 }
