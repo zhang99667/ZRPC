c802e0bb0f05114360538b5dd80d8740f5310fea
markz <=>
2024-10-25 20:49:57 +0800

    废弃，适配自定义协议

--- rpc-core/src/main/java/com/markz/rpccore/codec/body/RpcRequestDecoder.java
+++ rpc-core/src/main/java/com/markz/rpccore/codec/body/RpcRequestDecoder.java
@@ -1,4 +1,4 @@
-package com.markz.rpccore.codec;
+package com.markz.rpccore.codec.body;
 
 import com.markz.common.entity.rpc.RpcRequest;
 import com.markz.rpccore.serializer.fastjson.FastJsonSerializer;
@@ -13,6 +13,7 @@ import java.util.List;
  * 二次解码
  * 反序列化
  */
+@Deprecated
 public class RpcRequestDecoder extends MessageToMessageDecoder<ByteBuf> {
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf, List<Object> list) throws Exception {
