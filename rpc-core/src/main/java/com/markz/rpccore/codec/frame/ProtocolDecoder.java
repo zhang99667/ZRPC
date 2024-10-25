7d95f2ad96007589ce110755ea8318ed3277b83c
markz <=>
2024-10-25 09:43:16 +0800

    添加安全校验

--- rpc-core/src/main/java/com/markz/rpccore/codec/frame/ProtocolDecoder.java
+++ rpc-core/src/main/java/com/markz/rpccore/codec/frame/ProtocolDecoder.java
@@ -2,6 +2,7 @@ package com.markz.rpccore.codec.frame;
 
 import com.markz.rpccore.constant.ProtocolConstant;
 import com.markz.rpccore.protocol.ProtocolHeader;
+import com.markz.rpccore.protocol.ProtocolMessageTypeEnum;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
@@ -28,16 +29,50 @@ public class ProtocolDecoder extends ByteToMessageDecoder {
         long requestId = in.readLong(); // 读取请求 ID
         int bodyLength = in.readInt(); // 读取消息体长度
 
-        // 2. 构造协议头对象
+        // 2. 安全校验
+        if (securityCheck(ctx, in, magic, type, bodyLength)) return;
+
+        // 3. 构造协议头对象
         ProtocolHeader header = new ProtocolHeader(magic, type, requestId, bodyLength);
 
-        // 3. 确保消息体内容已到达
+        // 4. 确保消息体内容已到达
         if (in.readableBytes() < bodyLength) {
             in.resetReaderIndex();  // 如果消息体没有到达，重置读取位置，等待更多数据
             return;
         }
 
-        // 4. 将解码后的头信息传递到下一个处理器
+        // 5. 将解码后的头信息传递到下一个处理器
         out.add(header);
+    }
+
+    /**
+     * 安全校验
+     *
+     * @param ctx        ChannelHandlerContext
+     * @param in         ByteBuf
+     * @param magic      魔数
+     * @param type       消息类型
+     * @param bodyLength 消息体长度
+     * @return
+     */
+    private static boolean securityCheck(ChannelHandlerContext ctx, ByteBuf in, byte magic, byte type, int bodyLength) {
+        // 1. 校验魔数是否正确
+        if (magic != ProtocolConstant.PROTOCOL_MAGIC) {
+            ctx.close();  // 关闭连接，表示非法协议包
+            throw new IllegalArgumentException("Invalid magic number: " + magic);
+        }
+
+        // 2. 校验消息类型是否合法
+        if (ProtocolMessageTypeEnum.getEnumByKey(type) == null) {
+            ctx.close();  // 关闭连接，表示非法消息类型
+            throw new IllegalArgumentException("Invalid message type: " + type);
+        }
+
+        // 3. 校验消息体长度是否合理
+        if (bodyLength < 0 || bodyLength > in.readableBytes()) {
+            in.resetReaderIndex();  // 重置读指针，等待更多数据
+            return true;
+        }
+        return false;
     }
 }
