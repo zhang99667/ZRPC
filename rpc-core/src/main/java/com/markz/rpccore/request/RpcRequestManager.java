faff77eb3b218f24122b68b444f5d9ad978e1be8
markz <=>
2024-10-25 09:29:19 +0800

    改为发送 Protocol Message

--- rpc-core/src/main/java/com/markz/rpccore/request/RpcRequestManager.java
+++ rpc-core/src/main/java/com/markz/rpccore/request/RpcRequestManager.java
@@ -10,6 +10,9 @@ import com.markz.rpccore.holder.RpcConfigurationHolder;
 import com.markz.rpccore.loadbalancer.LoadBalance;
 import com.markz.rpccore.loadbalancer.LoadBalanceFactory;
 import com.markz.rpccore.model.ServiceProviderMeta;
+import com.markz.rpccore.protocol.ProtocolHeader;
+import com.markz.rpccore.protocol.ProtocolMessage;
+import com.markz.rpccore.protocol.ProtocolMessageTypeEnum;
 import com.markz.rpccore.registry.Registry;
 import com.markz.rpccore.registry.RegistryFactory;
 import lombok.extern.slf4j.Slf4j;
@@ -41,13 +44,24 @@ public class RpcRequestManager {
         // 1. 注册中心拿到服务列表
         Registry registry = RegistryFactory.getRegistry(registryConfig.getType());
         List<ServiceProviderMeta> serviceProviderMetaList = registry.serviceDiscovery(rpcRequest.getServiceName());
+
         if (!serviceProviderMetaList.isEmpty()) {
             // 2. load balance 选择出服务提供者
+            // TODO 暂时还不知道怎么把节点权重加进去
             LoadBalance loadBalancer = LoadBalanceFactory.getLoadBalancer(rpcConfig.getLoadbalancerType());
             ServiceProviderMeta serviceProviderMeta = loadBalancer.selectServer(serviceProviderMetaList);
 
-            // 3. 发送数据包
-            return getResponse(serviceProviderMeta, rpcRequest);
+            // 3. 封装协议
+            ProtocolMessage<RpcRequest> message = new ProtocolMessage<>();
+            ProtocolHeader protocolHeader = ProtocolHeader.builder()
+                    .requestId(rpcRequest.getRequestId())
+                    .type((byte) ProtocolMessageTypeEnum.REQUEST.ordinal())
+                    .build();
+            message.setProtocolHeader(protocolHeader);
+            message.setBody(rpcRequest);
+
+            // 4. 发送数据
+            return getResponse(serviceProviderMeta, message);
         } else {
             log.info("{} 服务当前不可用！", rpcRequest.getServiceName());
             return new RpcResponse();
@@ -58,12 +72,12 @@ public class RpcRequestManager {
      * 发送 RpcRequest 拿到 RpcResponse
      *
      * @param serviceProviderMeta 服务提供者元信息
-     * @param rpcRequest          RPC 请求实体类
+     * @param message             ProtocolMessage<RpcRequest>
      * @return RpcResponse
      */
-    private RpcResponse getResponse(ServiceProviderMeta serviceProviderMeta, RpcRequest rpcRequest) {
+    private RpcResponse getResponse(ServiceProviderMeta serviceProviderMeta, ProtocolMessage<RpcRequest> message) {
         String serviceHost = serviceProviderMeta.getServiceHost();
         int servicePort = serviceProviderMeta.getServicePort();
-        return nettyRpcClient.connectionAndSend(serviceHost, servicePort, rpcRequest);
+        return nettyRpcClient.connectionAndSend(serviceHost, servicePort, message);
     }
 }
