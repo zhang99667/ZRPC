2ca24f1badef6278a3699bcbb04bb0cf3825dfca
markz <=>
2024-10-25 09:27:52 +0800

    自定义协议

---
+++ rpc-core/src/main/java/com/markz/rpccore/protocol/ProtocolMessage.java
@@ -0,0 +1,24 @@
+package com.markz.rpccore.protocol;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 协议消息结构
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProtocolMessage<T> {
+
+    /**
+     * 协议头
+     */
+    private ProtocolHeader protocolHeader;
+
+    /**
+     * 协议体
+     */
+    private T body;
+}
