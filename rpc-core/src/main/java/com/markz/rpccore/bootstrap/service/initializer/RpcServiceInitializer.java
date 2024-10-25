ffbf91b9db06ebf408196fcad7d0a97d8d84e1a0
markz <=>
2024-10-25 20:51:09 +0800

    适配自定义协议

--- rpc-core/src/main/java/com/markz/rpccore/bootstrap/service/initializer/RpcServiceInitializer.java
+++ rpc-core/src/main/java/com/markz/rpccore/bootstrap/service/initializer/RpcServiceInitializer.java
@@ -1,9 +1,10 @@
 package com.markz.rpccore.bootstrap.service.initializer;
 
-import com.markz.rpccore.codec.RpcRequestDecoder;
-import com.markz.rpccore.codec.fastjson.FastJsonEncoder;
+import com.markz.rpccore.codec.body.RpcBodyDecoder;
+import com.markz.rpccore.codec.body.RpcBodyEncoder;
 import com.markz.rpccore.codec.frame.FrameDecoder;
-import com.markz.rpccore.codec.frame.FrameEncoder;
+import com.markz.rpccore.codec.protocol.ProtocolDecoder;
+import com.markz.rpccore.codec.protocol.ProtocolEncoder;
 import com.markz.rpccore.handler.RpcRequestHandler;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
@@ -13,12 +14,13 @@ public class RpcServiceInitializer extends ChannelInitializer {
     @Override
     protected void initChannel(Channel ch) {
         // outbound handler
-        ch.pipeline().addLast("FrameEncoder", new FrameEncoder()); // 封装成帧
-        ch.pipeline().addLast("Serializer", new FastJsonEncoder()); // 序列化
+        ch.pipeline().addLast("ProtocolEncoder", new ProtocolEncoder()); // 封装成帧
+        ch.pipeline().addLast("Serializer", new RpcBodyEncoder()); // 序列化
+
         // inbound handler
-        ch.pipeline().addLast("FrameDecoder", new FrameDecoder()); // 拆帧
-        // TODO 序列化器这里需要后续根据协议的type自动处理
-        ch.pipeline().addLast("Deserializer", new RpcRequestDecoder()); // 反序列化
+        ch.pipeline().addLast("FrameDecoder", new FrameDecoder()); // 定长解码器
+        ch.pipeline().addLast("ProtocolDecoder", new ProtocolDecoder()); // 信息校验
+        ch.pipeline().addLast("Deserializer", new RpcBodyDecoder()); // 反序列化
         ch.pipeline().addLast("RpcRequestHandler", new RpcRequestHandler()); // 处理 RpcRequest
     }
 }
