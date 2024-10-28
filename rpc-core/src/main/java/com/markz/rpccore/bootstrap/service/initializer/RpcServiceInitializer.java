dd49afff2d1e4428a9fca6e6c19774aa878a5d22
markz <zhangz1728@126.com>
2024-10-28 09:32:26 +0800

    添加idleHeartBeat机制

--- rpc-core/src/main/java/com/markz/rpccore/bootstrap/service/initializer/RpcServiceInitializer.java
+++ rpc-core/src/main/java/com/markz/rpccore/bootstrap/service/initializer/RpcServiceInitializer.java
@@ -6,6 +6,7 @@ import com.markz.rpccore.codec.frame.FrameDecoder;
 import com.markz.rpccore.codec.protocol.ProtocolDecoder;
 import com.markz.rpccore.codec.protocol.ProtocolEncoder;
 import com.markz.rpccore.handler.RpcRequestHandler;
+import com.markz.rpccore.handler.ServerIdleHeartBeatHandler;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 
@@ -15,12 +16,13 @@ public class RpcServiceInitializer extends ChannelInitializer {
     protected void initChannel(Channel ch) {
         // outbound handler
         ch.pipeline().addLast("ProtocolEncoder", new ProtocolEncoder()); // 封装成帧
-        ch.pipeline().addLast("Serializer", new RpcBodyEncoder()); // 序列化
+        ch.pipeline().addLast("Serializer", new RpcBodyEncoder());       // 序列化
 
         // inbound handler
-        ch.pipeline().addLast("FrameDecoder", new FrameDecoder()); // 定长解码器
-        ch.pipeline().addLast("ProtocolDecoder", new ProtocolDecoder()); // 信息校验
-        ch.pipeline().addLast("Deserializer", new RpcBodyDecoder()); // 反序列化
-        ch.pipeline().addLast("RpcRequestHandler", new RpcRequestHandler()); // 处理 RpcRequest
+        ch.pipeline().addLast("FrameDecoder", new FrameDecoder());                             // 定长解码器
+        ch.pipeline().addLast("ProtocolDecoder", new ProtocolDecoder());                       // 信息校验
+        ch.pipeline().addLast("Deserializer", new RpcBodyDecoder());                           // 反序列化
+        ch.pipeline().addLast("ServerIdleHeartBeatHandler", new ServerIdleHeartBeatHandler()); // 心跳机制
+        ch.pipeline().addLast("RpcRequestHandler", new RpcRequestHandler());                   // 处理 RpcRequest
     }
 }
