8f57b2750d5f491250b7751d8f6398ac349f3245
zhangz <zhangz1728@126.com>
2024-10-30 11:20:28 +0800

    修改服务端心跳保活逻辑，改为如果服务端长时间未收到心跳请求则断开channel

--- rpc-core/src/main/java/com/markz/rpccore/handler/ServerIdleHeartBeatHandler.java
+++ rpc-core/src/main/java/com/markz/rpccore/handler/ServerIdleHeartBeatHandler.java
@@ -1,56 +1,44 @@
 package com.markz.rpccore.handler;
 
-import cn.hutool.core.util.IdUtil;
-import com.markz.rpccore.protocol.HeartBeatMessage;
-import com.markz.rpccore.protocol.ProtocolHeader;
-import com.markz.rpccore.protocol.ProtocolMessage;
-import com.markz.rpccore.protocol.ProtocolMessageTypeEnum;
+import com.markz.rpccore.constant.HeartBeatConstant;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 服务端心跳保活
  */
 @Slf4j
-public class ServerIdleHeartBeatHandler extends SimpleChannelInboundHandler<HeartBeatMessage<Byte>> {
+public class ServerIdleHeartBeatHandler extends IdleStateHandler {
 
-    // public ServerHeartBeatHandler() {
-    //     super(HeartBeatConstant.READER_IDLE_TIME_SECONDS,
-    //             HeartBeatConstant.WRITER_IDLE_TIME_SECONDS,
-    //             HeartBeatConstant.ALL_IDLE_TIME_SECONDS,
-    //             TimeUnit.SECONDS);
-    // }
+    public ServerIdleHeartBeatHandler() {
+        super(HeartBeatConstant.READER_IDLE_TIME_SECONDS,
+                0,
+                HeartBeatConstant.ALL_IDLE_TIME_SECONDS,
+                TimeUnit.SECONDS);
+    }
 
     /**
      * 收到客户端心跳消息，给出相应
      *
      * @param ctx ChannelHandlerContext
-     * @param msg HeartBeatMessage
+     * @param evt Object
+     * @throws Exception Exception
      */
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, HeartBeatMessage msg) {
-        if (msg != null) {
-            ProtocolMessage<?> message = (ProtocolMessage<?>) msg;
-            if (message.getProtocolHeader().getType() == ProtocolMessageTypeEnum.HEART_BEAT.ordinal()) {
-                // 处理心跳消息
-                log.info("收到心跳消息");
-                HeartBeatMessage<Byte> heartBeatMessage = new HeartBeatMessage<Byte>();
-                ProtocolHeader header = ProtocolHeader.builder()
-                        .requestId(Long.parseLong(IdUtil.fastUUID()))
-                        .type((byte) ProtocolMessageTypeEnum.HEART_BEAT.ordinal())
-                        .build();
-                heartBeatMessage.setProtocolHeader(header);
-                heartBeatMessage.setBody(null);
-                ctx.writeAndFlush(heartBeatMessage).addListener(future -> {
-                    if (future.isSuccess()) {
-                        log.info("心跳发送成功...");
-                    } else {
-                        log.error("心跳发送失败...");
-                        throw new RuntimeException("heartBeatMessage 发送失败: {}" + future.cause().getMessage());
-                    }
-                });
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        if (evt instanceof IdleStateEvent) {
+            IdleStateEvent event = (IdleStateEvent) evt;
+            // 读空闲事件
+            if (event.state() == IdleState.READER_IDLE) {
+                log.debug("长时间没有收到消息了，断开连接");
+                ctx.close();
             }
         }
+        super.userEventTriggered(ctx, evt);
     }
 }
